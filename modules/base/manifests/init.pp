2f6184e26003ac23325444043b86a647b71313d2
Felix H. Dahlke <fhd@ubercode.de>
2012-11-06 14:33:30 +0100

    Install Mercurial

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,5 +1,9 @@
 class base {
-  package {
-    'emacs': ensure => present
+  package {'emacs': ensure => present}
+
+  package {'mercurial': ensure => present}
+  if $operatingsystem == 'OpenSuSE' {
+    package {'patterns-openSUSE-minimal_base-conflicts': ensure => absent}
+    Package['patterns-openSUSE-minimal_base-conflicts'] -> Package['mercurial']
   }
 }
