eaca8163db1c9780437c52d6d5420069d3a2051d
Felix H. Dahlke <fhd@ubercode.de>
2012-11-07 16:01:58 +0100

    Remove openSUSE specific package removal

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,9 +1,4 @@
 class base {
   package {'emacs': ensure => present}
-
   package {'mercurial': ensure => present}
-  if $operatingsystem == 'OpenSuSE' {
-    package {'patterns-openSUSE-minimal_base-conflicts': ensure => absent}
-    Package['patterns-openSUSE-minimal_base-conflicts'] -> Package['mercurial']
-  }
 }
