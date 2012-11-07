7d2cb79cc10c5ddc8c7d48e5a8c9e1a4cd8b39a5
Felix H. Dahlke <fhd@ubercode.de>
2012-11-07 16:53:21 +0100

    Install MySQL

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,3 +1,14 @@
 node default {
   include base, nginx
+
+  class {'mysql::server':
+    config_hash => {'root_password' => 'vagrant'}
+  }
+
+  mysql::db {'anwiki':
+    user => 'anwiki',
+    password => 'vagrant',
+    host => 'localhost',
+    grant => ['all']
+  }
 }
