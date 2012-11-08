308ba4abcaa34648e3df2b90938a6d92ab3bea8e
Felix H. Dahlke <fhd@ubercode.de>
2012-11-08 14:12:04 +0100

    Ensure that APT is updated first

--- modules/base/manifests/init.pp
+++ modules/base/manifests/init.pp
@@ -1,3 +1,9 @@
 class base {
+  stage { 'pre': before => Stage['main'] }
+  class {'apt':
+    stage => 'pre',
+    always_apt_update => true
+  }
+
   package {['mercurial', 'emacs']: ensure => present}
 }
