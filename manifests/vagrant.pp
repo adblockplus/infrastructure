fe8f99732018d1ad31692b5661ebb7289858925c
Felix H. Dahlke <fhd@ubercode.de>
2012-11-13 17:24:56 +0100

    Move adblockplus.org configuration to a dedicated module

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -1,20 +1,3 @@
 node default {
-  include base, nginx
-  
-  nginx::resource::vhost {'adblockplus.org':
-    ensure => present,
-    www_root => '/var/www/adblockplus.org'
-  }
-
-  class {'mysql::server':
-    config_hash => {'root_password' => 'vagrant'}
-  }
-
-  mysql::db {'anwiki':
-    user => 'anwiki',
-    password => 'vagrant',
-    host => 'localhost',
-    grant => ['all'],
-    require => Class['mysql::config']
-  }
+  include base, adblockplusorg
 }
---
+++ modules/adblockplusorg/manifests/init.pp
@@ -0,0 +1,20 @@
+class adblockplusorg {
+  class {'nginx':}
+
+  nginx::resource::vhost {'adblockplus.org':
+    ensure => present,
+    www_root => '/var/www/adblockplus.org'
+  }
+
+  class {'mysql::server':
+    config_hash => {'root_password' => 'vagrant'}
+  }
+
+  mysql::db {'anwiki':
+    user => 'anwiki',
+    password => 'vagrant',
+    host => 'localhost',
+    grant => ['all'],
+    require => Class['mysql::config']
+  }
+}
