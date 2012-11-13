5ea8d36713129f68acfdcab59db061bd8aae174f
Felix H. Dahlke <fhd@ubercode.de>
2012-11-13 17:29:03 +0100

    Add anwiki deploy script

--- modules/adblockplusorg/manifests/init.pp
+++ modules/adblockplusorg/manifests/init.pp
@@ -17,4 +17,16 @@ class adblockplusorg {
     grant => ['all'],
     require => Class['mysql::config']
   }
+
+  file {['/var/www', '/var/www/adblockplus.org',
+         '/var/www/adblockplus.org/anwiki', '/var/www/adblockplus.org/phproot']:
+    ensure => 'directory'
+  }
+
+  file {'/usr/local/bin/deploy-anwiki':
+    mode => 744,
+    owner => root,
+    group => root,
+    source => 'puppet:///modules/adblockplusorg/deploy-anwiki'
+  }
 }
