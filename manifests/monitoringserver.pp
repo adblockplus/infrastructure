7156a5f8e35eab0a4c232d4ddb957036ef17628c
Felix Dahlke <felix@adblockplus.org>
2013-01-16 09:10:17 +0100

    Rename htpasswd file

--- manifests/monitoringserver.pp
+++ manifests/monitoringserver.pp
@@ -2,6 +2,6 @@ node default {
   include base
 
   class {'nagios::server':
-    htpasswd_source => 'puppet:///modules/private/nagios3-htpasswd'
+    htpasswd_source => 'puppet:///modules/private/nagios-htpasswd'
   }
 }
