01c928b2cb0f3384309ff00150dc40c7dbb4db6f
Felix Dahlke <felix@adblockplus.org>
2013-01-17 07:47:07 +0100

    Set up hosts and hostgroups

--- manifests/monitoringserver.pp
+++ manifests/monitoringserver.pp
@@ -4,4 +4,9 @@ node default {
   class {'nagios::server':
     htpasswd_source => 'puppet:///modules/private/nagios-htpasswd'
   }
+
+  nagios_host {'10.8.0.97': use => 'generic-host'}
+  nagios_hostgroup {'all': members => '*'}
+  nagios_hostgroup {'ssh-servers': members => 'localhost, 10.8.0.97'}
+  nagios_hostgroup {'http-servers': members => 'localhost, 10.8.0.97'}
 }
