f64c5f12f935daedb4adf572dbf331f1bbe99e20
Felix Dahlke <felix@adblockplus.org>
2013-01-17 07:47:07 +0100

    Set up hosts and hostgroups

--- modules/nagios/manifests/server.pp
+++ modules/nagios/manifests/server.pp
@@ -9,4 +9,21 @@ class nagios::server($htpasswd_source) {
     group => root,
     source => $htpasswd_source
   }
+  
+  Nagios_host <| |> {
+    target => '/etc/nagios3/conf.d/hosts_nagios2.cfg',
+    notify => Service['nagios3']
+  }
+
+  file {'/etc/nagios3/conf.d/hosts_nagios2.cfg': mode => 644}
+
+  Nagios_hostgroup <| |> {
+    target => '/etc/nagios3/conf.d/hostgroups_nagios2.cfg',
+    notify => Service['nagios3']
+  }
+
+  service {'nagios3':
+    ensure => running,
+    enable => true
+  }
 }
