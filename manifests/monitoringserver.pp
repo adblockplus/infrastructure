160dd6b07b8fa8cabc48f5b5db5d6c1acb60e24f
Felix Dahlke <felix@adblockplus.org>
2013-01-17 18:04:48 +0100

    Specify all hosts and services

--- manifests/monitoringserver.pp
+++ manifests/monitoringserver.pp
@@ -5,8 +5,44 @@ node default {
     htpasswd_source => 'puppet:///modules/private/nagios-htpasswd'
   }
 
+  nagios_host {'localhost': use => 'generic-host'}
   nagios_host {'10.8.0.97': use => 'generic-host'}
+
   nagios_hostgroup {'all': members => '*'}
-  nagios_hostgroup {'ssh-servers': members => 'localhost, 10.8.0.97'}
   nagios_hostgroup {'http-servers': members => 'localhost, 10.8.0.97'}
+
+  nagios_service {'disk-space':
+    use => 'generic-service',
+    hostgroup_name => 'all',
+    service_description => 'Disk Space',
+    check_command => 'check_all_disks!20%!10%'
+  }
+
+  nagios_service {'total-processes':
+    use => 'generic-service',
+    hostgroup_name => 'all',
+    service_description => 'Total Processes',
+    check_command => 'check_procs!250!400'
+  }
+
+  nagios_service {'current-load':
+    use => 'generic-service',
+    hostgroup_name => 'all',
+    service_description => 'Current Load',
+    check_command => 'check_load!5.0!4.0!3.0!10.0!6.0!4.0'
+  }
+
+  nagios_service {'ssh':
+    use => 'generic-service',
+    hostgroup_name => 'all',
+    service_description => 'SSH',
+    check_command => 'check_ssh'
+  }
+
+  nagios_service {'http':
+    use => 'generic-service',
+    hostgroup_name => 'http-servers',
+    service_description => 'HTTP',
+    check_command => 'check_http'
+  }
 }
