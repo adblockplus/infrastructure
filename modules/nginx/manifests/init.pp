a541bce1a512e9d53a244d0436756cbfbefed144
Andrey Novikov <andrey@adblockplus.org>
2013-01-18 12:56:59 +0400

    Added nginx module

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,52 @@
+class nginx (
+    $service_subscribe = undef
+  ){
+
+  package {'nginx': ensure => 'present'}
+
+  user {'nginx':
+    ensure => present,
+	uid => 106,
+	gid => users,
+    comment => 'User for nginx',
+    home => '/var/lib/nginx',
+	shell => '/bin/false',
+	managehome => false
+  }
+
+  file {'/srv/log':
+    ensure => directory,
+    owner => root,
+    group => root,
+    mode => 644
+  }
+  
+  file {'/etc/nginx/nginx.conf':
+    mode => 644,
+    owner => root,
+    group => root,
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx']
+  }
+
+  file {'/etc/logrotate.d/nginx':
+    mode => 644,
+    owner => root,
+    group => root,
+    source => 'puppet:///modules/nginx/logrotate',
+    require => File['/etc/nginx/nginx.conf']
+  }
+  
+  service {'nginx':
+    ensure => running,
+    enable => true,
+    hasrestart => true,
+    hasstatus => true,
+    require => [
+                 File['/srv/log'],
+                 File['/etc/nginx/nginx.conf'],
+                 User['nginx']
+               ],
+    subscribe => $service_subscribe
+  }  
+}
