72b83ee7285975049e38373e1dace630a57e2f70
Andrey Novikov <andrey@adblockplus.org>
2013-01-21 17:38:07 +0400

    Added sitescripts module

---
+++ modules/sitescripts/manifests/init.pp
@@ -0,0 +1,18 @@
+class sitescripts (
+    $sitescriptsini_source = undef
+  ){
+
+  file {'/etc/sitescripts.ini':
+    mode => 644,
+    owner => root,
+    group => root,
+    source => $sitescriptsini_source
+  }
+
+  exec { "fetch_sitescripts":
+    command => "hg clone https://hg.adblockplus.org/sitescripts /opt/sitescripts",
+    path    => [ "/usr/bin/", "/bin/" ],
+    require => Package['mercurial'],
+	refreshonly => true
+  }
+}
