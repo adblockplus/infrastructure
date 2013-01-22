e6878d47adcc63abab773be3de46babb596de16a
Felix Dahlke <felix@adblockplus.org>
2013-01-22 10:30:48 +0100

    Don't hard code the host name
    --HG--
    extra : rebase_source : 1b0abee5d548462783e8877a3b8f6207a16d6342

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -2,7 +2,7 @@ node default {
   class {'hosts':
     hosts => {
       'localhost' => '127.0.0.1',
-      'precise64' => '127.0.0.1',
+      "${hostname}" => '127.0.0.1',
       'www.adblockplus.org' => '10.8.0.97',
       'monitoring.adblockplus.org' => '10.8.0.98'
     }
