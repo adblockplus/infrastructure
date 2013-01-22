a35b2190432fa0c3463043c6af5406a3a84b2810
Andrey Novikov <andrey@adblockplus.org>
2013-01-22 12:41:39 +0400

    Removed custom nginx user

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -4,16 +4,6 @@ class nginx (
 
   package {'nginx': ensure => 'present'}
 
-  user {'nginx':
-    ensure => present,
-    uid => 106,
-    gid => users,
-    comment => 'User for nginx',
-    home => '/var/lib/nginx',
-    shell => '/bin/false',
-    managehome => false
-  }
-
   file {'/etc/nginx/nginx.conf':
     mode => 644,
     owner => root,
@@ -35,10 +25,7 @@ class nginx (
     enable => true,
     hasrestart => true,
     hasstatus => true,
-    require => [
-                 File['/etc/nginx/nginx.conf'],
-                 User['nginx']
-               ],
+    require => File['/etc/nginx/nginx.conf'],
     subscribe => $service_subscribe
   }  
 }
