a77f64c1ba01aa61b79c45cde52dc7994e434208
Felix Dahlke <felix@adblockplus.org>
2013-01-23 16:24:14 +0100

    Monitor server3

--- manifests/filterserver.pp
+++ manifests/filterserver.pp
@@ -1,3 +1,7 @@
 node 'server3' {
   include base, filterserver
+
+  class {'nagios::client':
+    server_address => 'monitoring.adblockplus.org'
+  }
 }
--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -4,7 +4,8 @@ node default {
       'localhost' => '127.0.0.1',
       "${hostname}" => '127.0.0.1',
       'www.adblockplus.org' => '10.8.0.97',
-      'monitoring.adblockplus.org' => '10.8.0.98'
+      'monitoring.adblockplus.org' => '10.8.0.98',
+      'server_3.adblockplus.org' => '10.8.0.99'
     }
   }
 }
