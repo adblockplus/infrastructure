976f6414068006b763caa214cc92c7bd320358e0
Felix Dahlke <felix@adblockplus.org>
2013-01-26 08:27:45 +0100

    Configure contacts/contactgroups

--- manifests/monitoringserver.pp
+++ manifests/monitoringserver.pp
@@ -11,6 +11,22 @@ node 'server4' {
     admins => ['fhd']
   }
 
+  nagios_contact {'felix':
+    alias => 'Felix H. Dahlke',
+    service_notification_period => '24x7',
+    host_notification_period => '24x7',
+    service_notification_options => 'w,u,c,r',
+    host_notification_options => 'd,r',
+    service_notification_commands => 'notify-service-by-email',
+    host_notification_commands => 'notify-host-by-email',
+    email => 'felix@adblockplus.org'
+  }
+
+  nagios_contactgroup {'admins':
+    alias => 'Nagios Administrators',
+    members => 'felix'
+  }
+
   nagios_host {'localhost': use => 'generic-host'}
   nagios_host {'www.adblockplus.org': use => 'generic-host'}
   nagios_host {'server_3.adblockplus.org': use => 'generic-host'}
