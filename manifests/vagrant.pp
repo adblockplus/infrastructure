fb20b9d686b6f47326fd29f5201ef9fa49ec10cd
Felix Dahlke <felix@adblockplus.org>
2013-02-01 15:40:30 +0100

    Add missing servers
    --HG--
    extra : rebase_source : b901a61adb0a4687bf6409bd63b5c2caa038d5f5

--- manifests/vagrant.pp
+++ manifests/vagrant.pp
@@ -7,7 +7,11 @@ node default {
       'monitoring.adblockplus.org' => '10.8.0.98',
       'server_3.adblockplus.org' => '10.8.0.99',
       'server_4.adblockplus.org' => '10.8.0.98',
-      'server_5.adblockplus.org' => '10.8.0.100'
+      'server_5.adblockplus.org' => '10.8.0.100',
+      'server_6.adblockplus.org' => '10.8.0.101',
+      'server_7.adblockplus.org' => '10.8.0.102',
+      'server_8.adblockplus.org' => '10.8.0.103',
+      'server_9.adblockplus.org' => '10.8.0.104'
     }
   }
 }
