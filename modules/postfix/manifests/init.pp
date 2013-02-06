ba486fa944a8cdb3886873c6154bfff1210edae4
Wladimir Palant <trev@adblockplus.org>
2013-02-06 09:18:49 +0100

    Add Postfix configuration to the repository, make Postfix listen on local interfaces only

---
+++ modules/postfix/manifests/init.pp
@@ -0,0 +1,20 @@
+class postfix {
+  package {'postfix': ensure => present}
+
+  file {'/etc/postfix/main.cf':
+    ensure => present,
+    owner => root,
+    group => root,
+    mode => 0644,
+    source => 'puppet:///modules/postfix/main.cf',
+    require => Package['postfix'],
+    notify => Service['postfix']
+  }
+
+  service {'postfix':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    hasrestart => true
+  }
+}
