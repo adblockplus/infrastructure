88c4394b17d73e7f7c1a0afd9a148ff547ace2fe
Wladimir Palant <trev@adblockplus.org>
2013-02-28 12:10:31 +0100

    Configure secret_token (assuming it's actually used anywhere) and document other private settings
    --HG--
    branch : intraforum

--- modules/private-stub/manifests/discourse.pp
+++ modules/private-stub/manifests/discourse.pp
@@ -1,5 +1,13 @@
 class private::discourse {
-  $secret = '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
+  # Secret token for session generation (initializers/secret_token.rb), generated via rake secret
+  $secret = '12f125a3f0bf338f6dc1382786537d36f279e011cc91390ed57691725924c31ddf778e30eb7cc686361c0815202ec5ec40f8b6627ca960732eb1c0e6769db5fa'
+
+  # Email addresses of accounts that will get admin privileges (have to be confirmed)
   $admins = ['test1@example.com', 'test2@example.com']
+
+  # AirBrake API key
   $airbrake_key = '0123456789abcdef0123456789abcdef'
+
+  # Secret key for the secret_token site setting, generated via rake secret
+  $cookie_secret = '1475f0c65f1753966844cd520cbc85f95c2a71e22ae2ddde34df0a78a2c63a70f9b241686c2dba5562126a5f3c840ec4f8f7e1a6fc64af2c4ccfdd85de5d3b4e'
 }
