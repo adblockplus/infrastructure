4bd0245c2794249314e5e7da09731c38260acfff
Wladimir Palant <trev@adblockplus.org>
2014-03-07 15:27:49 +0100

    Added more filter servers (not added to stats processing yet)

--- manifests/filterserver.pp
+++ manifests/filterserver.pp
@@ -1,4 +1,4 @@
-node 'server5', 'server6', 'server7', 'server11', 'server12', 'server15', 'server19', 'filter1', 'filter2', 'filter3', 'filter4', 'filter5', 'filter6', 'notification1', 'notification2' {
+node 'server5', 'server6', 'server7', 'server11', 'server12', 'server15', 'server19', 'filter1', 'filter2', 'filter3', 'filter4', 'filter5', 'filter6', 'filter7', 'filter8', 'filter9', 'filter10', 'filter11', 'filter12', 'filter13', 'filter14', 'filter15', 'filter16', 'filter17', 'filter18', 'notification1', 'notification2' {
   include base, statsclient
 
   class {'filterserver':
