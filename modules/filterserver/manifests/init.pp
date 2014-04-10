73e223d2c9a8be8ad3e558c2352c68c022978d80
Wladimir Palant <trev@adblockplus.org>
2014-04-10 17:26:52 +0200

    Issue 254 - Using specific certificate again

--- modules/filterserver/manifests/init.pp
+++ modules/filterserver/manifests/init.pp
@@ -42,8 +42,8 @@ class filterserver($is_default = false) {
     alt_names => 'easylist-msie.adblockplus.org',
     source => 'puppet:///modules/filterserver/site.conf',
     is_default => $is_default,
-    certificate => 'adblockplus.org_sslcert.pem',
-    private_key => 'adblockplus.org_sslcert.key',
+    certificate => 'easylist-downloads.adblockplus.org_sslcert.pem',
+    private_key => 'easylist-downloads.adblockplus.org_sslcert.key',
     log => 'access_log_easylist_downloads'
   }
 
