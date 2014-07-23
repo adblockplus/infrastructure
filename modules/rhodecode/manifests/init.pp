bb154bbcd81c400737c1a63776b966be1124bab4
AAlvz <alfonso@alvz.mx>
2014-07-23 09:39:30 -0500

    Fixed syntax problems

--- modules/rhodecode/manifests/init.pp
+++ modules/rhodecode/manifests/init.pp
@@ -1,7 +1,4 @@
-class rhodecode(
-      $user,
-) inherits private::rhodecode{
-
+class rhodecode($user) inherits private::rhodecode {
   class {'nginx':
     worker_processes => 1,
     worker_connections => 500
@@ -13,26 +10,26 @@ class rhodecode(
   }
 
   file {'/etc/nginx/conf.d/default.conf':
-       ensure => absent,
-       require => [
-                   Exec["Install_RhodeCode_$user"],
-                   Package['nginx']
-                  ],
-       notify => Service['nginx'],
+    ensure => absent,
+    require => [
+                Exec["install_rhodeCode_$user"],
+                Package['nginx']
+               ],
+    notify => Service['nginx'],
   }
 
   group {$user:
-        ensure => "present",
-        name => $user,
+    ensure => "present",
+    name => $user,
   }
 
   user {$user:
-       groups => ['root'],
-       gid => $user,
-       ensure => 'present',
-       managehome => true,
-       comment => 'This user was created by Puppet',
-       require => Group[$user]
+    groups => ['root'],
+    gid => $user,
+    ensure => 'present',
+    managehome => true,
+    comment => 'This user was created by Puppet',
+    require => Group[$user]
   }
 
   file {"/home/$user/rhodecode":
@@ -42,35 +39,35 @@ class rhodecode(
     require => User[$user],
   }
 
-  exec {"Download_installer":
-       command => "/usr/bin/wget https://rhodecode.com/dl/rhodecode-installer.py",
-       cwd => "/home/$user/rhodecode",
-       creates => "/home/$user/rhodecode/rhodecode-installer.py",
-       require => File["/home/$user/rhodecode"],
+  exec {"download_installer":
+    command => "/usr/bin/wget https://rhodecode.com/dl/rhodecode-installer.py",
+    cwd => "/home/$user/rhodecode",
+    creates => "/home/$user/rhodecode/rhodecode-installer.py",
+    require => File["/home/$user/rhodecode"],
   }
 
   file {"/home/$user/rhodecode/noninteractive.ini" :
-       ensure => file,
-       mode => 644,
-       content => template("rhodecode/noninteractive.conf.erb"),
+    ensure => file,
+    mode => 644,
+    content => template("rhodecode/noninteractive.conf.erb"),
   }
 
-  exec {"Install_RhodeCode_$user":
-       command => "/usr/bin/sudo /usr/bin/python rhodecode-installer.py -n > installation.log 2>&1",
-       user => "root",
-       provider => "shell",
-       cwd => "/home/$user/rhodecode",
-       require => [
-                  File["/home/$user/rhodecode/noninteractive.ini"],
-                  Exec["Download_installer"]
-                  ],
-       timeout => 1200,
+  exec {"install_rhodeCode_$user":
+    command => "/usr/bin/sudo /usr/bin/python rhodecode-installer.py -n > installation.log 2>&1",
+    user => "root",
+    provider => "shell",
+    cwd => "/home/$user/rhodecode",
+    require => [
+               File["/home/$user/rhodecode/noninteractive.ini"],
+               Exec["download_installer"]
+               ],
+    timeout => 1200,
   }
 
   service {"rhodecode": 
-       ensure => "running",
-       enable => true,
-       require => Exec["Install_RhodeCode_$user"],
+    ensure => "running",
+    enable => true,
+    require => Exec["install_rhodeCode_$user"],
   }
 
 }
