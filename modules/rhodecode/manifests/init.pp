a7a65499944c26b1b4800cbc66dbc892305e523a
AAlvz <alfonso@alvz.mx>
2014-08-09 14:06:36 -0500

    fixed indentation

--- modules/rhodecode/manifests/init.pp
+++ modules/rhodecode/manifests/init.pp
@@ -14,17 +14,17 @@ class rhodecode(
   }
 
   group {$user:
-        ensure => "present",
-        name => $user,
+    ensure => "present",
+    name => $user,
   }
 
   user {$user:
-       groups => ['root'],
-       gid => $user,
-       ensure => 'present',
-       managehome => true,
-       comment => 'This user was created by Puppet',
-       require => Group[$user]
+    groups => ['root'],
+    gid => $user,
+    ensure => 'present',
+    managehome => true,
+    comment => 'This user was created by Puppet',
+    require => Group[$user]
   }
 
   file {"/home/$user/rhodecode":
@@ -35,34 +35,34 @@ class rhodecode(
   }
 
   exec {"Download_installer":
-       command => "/usr/bin/wget https://rhodecode.com/dl/rhodecode-installer.py",
-       cwd => "/home/$user/rhodecode",
-       creates => "/home/$user/rhodecode/rhodecode-installer.py",
-       require => File["/home/$user/rhodecode"],
+    command => "/usr/bin/wget https://rhodecode.com/dl/rhodecode-installer.py",
+    cwd => "/home/$user/rhodecode",
+    creates => "/home/$user/rhodecode/rhodecode-installer.py",
+    require => File["/home/$user/rhodecode"],
   }
 
   file {"/home/$user/rhodecode/noninteractive.ini" :
-       ensure => file,
-       mode => 644,
-       content => template("rhodecode/noninteractive.conf.erb"),
+    ensure => file,
+    mode => 644,
+    content => template("rhodecode/noninteractive.conf.erb"),
   }
 
   exec {"Install_RhodeCode_$user":
-       command => "/usr/bin/sudo /usr/bin/python rhodecode-installer.py -n > installation.log 2>&1",
-       user => "root",
-       provider => "shell",
-       cwd => "/home/$user/rhodecode",
-       require => [
-                  File["/home/$user/rhodecode/noninteractive.ini"],
-                  Exec["Download_installer"]
-                  ],
-       timeout => 1200,
+    command => "/usr/bin/sudo /usr/bin/python rhodecode-installer.py -n > installation.log 2>&1",
+    user => "root",
+    provider => "shell",
+    cwd => "/home/$user/rhodecode",
+    require => [
+      File["/home/$user/rhodecode/noninteractive.ini"],
+      Exec["Download_installer"]
+    ],
+    timeout => 1200,
   }
 
   service {"rhodecode": 
-       ensure => "running",
-       enable => true,
-       require => Exec["Install_RhodeCode_$user"],
+    ensure => "running",
+    enable => true,
+    require => Exec["Install_RhodeCode_$user"],
   }
 
 }
