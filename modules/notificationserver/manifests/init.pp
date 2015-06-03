60e97497de77b1493f100c11f037b71b6d3c2862
Felix Dahlke <felix@adblockplus.org>
2015-06-03 10:25:51 +0200

    Back out revision 4fbd0c2516eb (Issue 2277 - Use the new notification.json handler) - Was pushed accidentally

--- modules/notificationserver/manifests/init.pp
+++ modules/notificationserver/manifests/init.pp
@@ -7,35 +7,46 @@ class notificationserver($is_default = false) {
     }
   }
 
+  if !defined(File['/var/www']) {
+    file {'/var/www':
+      ensure => directory,
+      owner => nginx,
+      mode => 0755,
+      require => Package['nginx']
+    }
+  }
+
   class {'sitescripts':
     sitescriptsini_source => 'puppet:///modules/notificationserver/sitescripts.ini'
   }
 
-  exec {'fetch_notifications':
-    command => 'hg clone --noupdate https://hg.adblockplus.org/notifications /opt/notifications && chown -R nginx /opt/notifications',
-    path => ['/usr/bin/', '/bin/'],
+  file {'/var/www/notification':
+    ensure => directory,
+    owner => nginx,
+    mode => 0755,
+    require => Package['nginx']
+  }
+
+  exec { "fetch_notifications":
+    command => "hg clone --noupdate https://hg.adblockplus.org/notifications /opt/notifications && chown -R nginx /opt/notifications",
+    path => ["/usr/bin/", "/bin/"],
     require => [
       Package['mercurial'],
       Package['nginx'],
     ],
-    onlyif => 'test ! -d /opt/notifications'
+    onlyif => "test ! -d /opt/notifications"
   }
 
-  include spawn-fcgi
-  package {'python-flup':}
-
-  spawn-fcgi::pool {'multiplexer':
+  cron {"update_notifications":
     ensure => present,
-    fcgi_app => '/opt/sitescripts/multiplexer.fcgi',
-    socket => '/tmp/multiplexer-fastcgi.sock',
-    mode => '0666',
-    user => 'nginx',
-    children => 1,
+    command => "python -m sitescripts.management.bin.generateNotifications",
+    environment => ['MAILTO=admins@adblockplus.org,root', 'PYTHONPATH=/opt/sitescripts'],
+    user => nginx,
+    minute => '*/10',
     require => [
-      Exec['fetch_sitescripts'],
-      Exec['fetch_notifications'],
-      Package['python-flup']
-    ]
+      Exec["fetch_notifications"],
+      Exec["fetch_sitescripts"]
+    ],
   }
 
   File {
@@ -46,7 +57,6 @@ class notificationserver($is_default = false) {
 
   nginx::hostconfig{'notification.adblockplus.org':
     source => 'puppet:///modules/notificationserver/site.conf',
-    global_config => template('notificationserver/global.conf.erb'),
     is_default => $is_default,
     certificate => 'easylist-downloads.adblockplus.org_sslcert.pem',
     private_key => 'easylist-downloads.adblockplus.org_sslcert.key',
