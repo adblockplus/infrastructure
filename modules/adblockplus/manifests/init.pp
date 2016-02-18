fd3236984d8cfb4bef31c7a5ba9b399617715c6d
Matze <mathias@adblockplus.org>
2016-02-18 13:41:46 +0100

    Issue 3574 - Introduce parameter $adblockplus::authority

--- modules/adblockplus/manifests/init.pp
+++ modules/adblockplus/manifests/init.pp
@@ -6,6 +6,10 @@
 #
 # === Parameters:
 #
+# [*authority*]
+#   The authorative domain or zone associated with the current environment,
+#   similar to the deprecated and soon to be removed $base::zone.
+#
 # [*users*]
 #   A hash of adblockplus::user $name => $parameter items to set up in this
 #   context, i.e. via Hiera.
@@ -21,11 +25,14 @@
 #   }
 #
 class adblockplus (
+  $authority = hiera('adblockplus::authority', 'adblockplus.org'),
   $users = hiera_hash('adblockplus::users', {}),
 ) {
 
-  # See https://issues.adblockplus.org/ticket/3574#comment:4
-  include base
+  # See https://issues.adblockplus.org/ticket/3574#comment:8
+  class {'base':
+    zone => $authority,
+  }
 
   # Used as internal constant within adblockplus::* resources
   $directory = '/var/adblockplus'
