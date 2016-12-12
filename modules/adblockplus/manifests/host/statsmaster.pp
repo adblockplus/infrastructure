faeea3d0d123c22564acd89e748639ee9ed5b099
Matze <mathias@adblockplus.org>
2016-12-12 12:07:58 +0100

    Issue 3638 - Generate statsmaster filter mirror configuration with concat

--- modules/adblockplus/manifests/host/statsmaster.pp
+++ modules/adblockplus/manifests/host/statsmaster.pp
@@ -5,6 +5,25 @@
 #
 define adblockplus::host::statsmaster {
 
+  $ensure = getparam(Adblockplus::Host[$title], 'ensure')
+  $role = getparam(Adblockplus::Host[$title], 'role')
+
   realize(Host[$title])
   realize(Sshkey[$title])
+
+  # https://issues.adblockplus.org/ticket/3638#comment:17
+  if ($role == 'filterserver') and ($ensure == 'present') {
+
+    include sitescripts
+    $fqdn = getparam(Adblockplus::Host[$title], 'fqdn')
+
+    sitescripts::configfragment {"mirror#$title":
+      content => join([
+        "# Filter mirror $fqdn",
+        "mirror_$name=subscription ssh://stats@$fqdn/access_log_easylist_downloads.1.gz",
+        "mirror_n_$name=notification ssh://stats@$fqdn/access_log_notification.1.gz",
+        ""
+      ], "\n"),
+    }
+  }
 }
