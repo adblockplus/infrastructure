f3ba9abc2cfc16f8b40481aa7df062153d02f8c5
Matze <mathias@adblockplus.org>
2016-12-13 08:59:49 +0100

    Issue 4728 - Remove hard-coded references from statsmaster sitescripts.ini template

--- modules/adblockplus/manifests/host/statsmaster.pp
+++ modules/adblockplus/manifests/host/statsmaster.pp
@@ -11,19 +11,44 @@ define adblockplus::host::statsmaster {
   realize(Host[$title])
   realize(Sshkey[$title])
 
-  # https://issues.adblockplus.org/ticket/3638#comment:17
-  if ($role == 'filterserver') and ($ensure == 'present') {
+  if $ensure == 'present' {
 
     include sitescripts
     $fqdn = getparam(Adblockplus::Host[$title], 'fqdn')
 
-    sitescripts::configfragment {"mirror#$title":
-      content => join([
-        "# Filter mirror $fqdn",
-        "mirror_$name=subscription ssh://stats@$fqdn/access_log_easylist_downloads.1.gz",
-        "mirror_n_$name=notification ssh://stats@$fqdn/access_log_notification.1.gz",
-        ""
-      ], "\n"),
+    # https://issues.adblockplus.org/ticket/3638#comment:17
+    if $role == 'filterserver' {
+
+      sitescripts::configfragment {"mirror#$title":
+        content => join([
+          "# Filter mirror $fqdn",
+          "mirror_$name=subscription ssh://stats@$fqdn/access_log_easylist_downloads.1.gz",
+          "mirror_n_$name=notification ssh://stats@$fqdn/access_log_notification.1.gz",
+          ""
+        ], "\n"),
+      }
+    }
+
+    # https://issues.adblockplus.org/ticket/4728
+    if $role == 'downloadserver' {
+
+      sitescripts::configfragment {"mirror#$title":
+        content => join([
+          "# Download mirror $fqdn",
+          "mirror_$name=download ssh://stats@$fqdn/access_log_downloads.1.gz",
+          ""
+        ], "\n"),
+      }
+    }
+    elsif $role == 'updateserver' {
+
+      sitescripts::configfragment {"mirror#$title":
+        content => join([
+          "# Update mirror $fqdn",
+          "mirror_$name=update ssh://stats@$fqdn/access_log_update.1.gz",
+          ""
+        ], "\n"),
+      }
     }
   }
 }
