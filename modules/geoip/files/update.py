b1ddcd8e3af2d5d61e28336de9db89994bcc6c80
Matze <mathias@adblockplus.org>
2018-05-18 04:06:14 +0200

    #11294 - Include GeoIP2 downloads with geoip module

--- modules/geoip/files/update.py
+++ modules/geoip/files/update.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import contextlib
+import os
+import tarfile
 import urllib
 import zlib
 
@@ -9,6 +12,9 @@ downloads = {
 
     '/usr/share/GeoIP/GeoIPCity.dat': 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz',
     '/usr/share/GeoIP/GeoIPCityv6.dat': 'http://geolite.maxmind.com/download/geoip/database/GeoLiteCityv6-beta/GeoLiteCityv6.dat.gz',
+
+    '/usr/share/GeoIP/GeoIP2.tar': 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz',
+    '/usr/share/GeoIP/GeoIP2City.tar': 'http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz',
 }
 
 for dest, source in downloads.iteritems():
@@ -16,3 +22,14 @@ for dest, source in downloads.iteritems():
     with open(dest, "wb") as f:
         # wbit parameter value isn't properly documented, see https://stackoverflow.com/a/22310760/785541
         f.write(zlib.decompress(data, zlib.MAX_WBITS | 16))
+
+for path in filter(lambda key: key.endswith('.tar'), downloads.keys()):
+    dirname = os.path.dirname(path)
+    with contextlib.closing(tarfile.open(path)) as archive:
+        archive.extractall(path=dirname)
+        is_database = lambda info: info.name.endswith('.mmdb')
+        for database in filter(is_database, archive.getmembers()):
+            target = os.path.join(dirname, os.path.basename(database.name))
+            intermediate = target + '.new'
+            os.symlink(database.name, intermediate)
+            os.rename(intermediate, target)
