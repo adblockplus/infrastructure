e40dc85641729d3a55a88ca3cbf02dfaba673d2c
Francisco López <poz2k4444@gmail.com>
2019-05-17 04:47:42 -0500

    Provide incremental filter list updates

---
+++ modules/filtermaster/manifests/iflu.pp
@@ -0,0 +1,45 @@
+# == Class: filtermaster::iflu
+#
+#  This class manages the asociated resources for the incremental filter
+#  lists updates (aka iflu)
+#
+# === Parameters:
+#
+# [*ensure*]
+#   General resource policy, i.e. "present" or "absent".
+#
+# [*filterlists*]
+#   A hash of filtermaster::iflu::filterlist items to set up in this
+#   context, via Hiera, exclusively.
+#
+class filtermaster::iflu (
+  $ensure = 'present',
+  $filterlists = {},
+) {
+
+  # python-abp contains a script called fldiff that will find the diff between
+  # the latest filter list, and any number of previous filter lists.
+  # from: https://github.com/adblockplus/python-abp
+  ensure_resource('package', ['python-abp'], {
+    ensure => '0.1.2',
+    provider => 'pip',
+    require => Package['python-pip'],
+  })
+
+  $data_directory = '/home/rsync/generated/data'
+  $archive_directory = "${data_directory}/archive"
+  $diff_directory = "${data_directory}/diff"
+
+  $base_directories = [
+    $data_directory,
+    $archive_directory,
+    $diff_directory,
+  ]
+
+  ensure_resource('file', $base_directories, {
+    ensure => ensure_directory_state($ensure),
+    owner => 'rsync',
+  })
+
+  create_resources('filtermaster::iflu::filterlist', $filterlists)
+}
---
+++ modules/filtermaster/manifests/iflu/filterlist.pp
@@ -0,0 +1,64 @@
+# == Type: filtermaster::iflu::filterlist
+#
+# Manage resrouces associated to a specific filter list in the
+# incremental updates context.
+#
+# === Parameters:
+#
+# [*cron*]
+#   Default options for Cron['filtermaster::iflu::filterlist'], e.g. $minute,
+#   $monthday etc.
+#
+# [*ensure*]
+#   General resource policy, i.e. "present" or "absent".
+#
+# [*hook*]
+#
+#  A command to execute when Cron['filtermaster::iflu::filterlist'] has
+#  succeeded, optional.
+#
+define filtermaster::iflu::filterlist (
+  $cron = {},
+  $ensure = 'present',
+  $hook = undef,
+) {
+
+  Cron {
+    environment => concat(hiera('cron::environment', []), [
+      'PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+    ]),
+  }
+
+  $archive_directory = "$filtermaster::iflu::archive_directory/$title"
+  $diff_directory = "$filtermaster::iflu::diff_directory/$title"
+
+  $directories = [
+    $archive_directory,
+    $diff_directory,
+  ]
+
+  ensure_resource('file', $directories, {
+    ensure => ensure_directory_state($ensure),
+    owner => 'rsync',
+  })
+
+  # According to the specification of incremental filter lists updates, the
+  # diffs should be up to two days and no more. For more info see:
+  # https://gitlab.com/eyeo/devops/python-abp/wikis/iflu-0.1
+  $script = join([
+    "cd ${filtermaster::iflu::data_directory}",
+    "cp ${title}.txt $(date +${archive_directory}/${title}_\\%d_\\%m_\\%Y_\\%H_\\%M.txt)",
+    "find ${archive_directory} -type f -mmin +2880 -delete",
+    "find ${diff_directory}/ -type f -delete",
+    "fldiff -o ${diff_directory} ${title}.txt ${archive_directory}/*",
+  ], '&&')
+
+  ensure_resource('cron', {"iflu#$title" => $cron}, merge({
+    command => $hook ? {undef => $script, default => "$script && $hook"},
+    ensure => $ensure,
+    hour => '*/1',
+    minute => 0,
+    user => 'rsync',
+  }, $cron))
+
+}
