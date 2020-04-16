adblockplus/infrastructure

commit 0639b91b983e8c5c64555a03db367e316421cfd6

#7320 - Introduce static website role

modules/adblockplus/manifests/web/static.pp
@@ -1,0 +1,123 @@
+# == Class: adblockplus::web::static
+#
+# Manage a simple Nginx-based webserver for static content
+# that uses a customizable deployment script to e.g. fetch the content
+# from a repository server (ref. http://hub.eyeo.com/issues/4523)
+#
+# === Parameters:
+#
+# [*domain*]
+#   The domain name for the website.
+#
+# [*ssl_certificate*]
+#   The name of the SSL certificate file within modules/private/files, if any.
+#   Requires a private_key as well.
+#
+# [*ssl_private_key*]
+#   The name of the private key file within modules/private/files, if any.
+#   Requires a certificate as well.
+#
+# [*ensure*]
+#   Whether to set up the website or not, e.g. "asbsent" or "present".
+#
+# [*deploy_user*]
+#   User that will be used to issue commands.
+#
+# [*deploy_user_authorized_keys*]
+#   Array of public keys that will have access to ssh commands
+#
+# [*hooks*]
+#   Hash of adblockplus::web::static::hook items to set up in this context.
+#
+# === Examples:
+#
+#   class {'adblockplus::web::static':
+#     domain => 'help.eyeo.com',
+#     hooks => {
+#       uname => {
+#         file => {
+#           content => 'uname -a',
+#         },
+#       },
+#       uptime => {
+#         file => {
+#           target => '/usr/bin/uptime',
+#           ensure => 'link',
+#         },
+#       },
+#     },
+#   }
+#
+class adblockplus::web::static (
+  $domain,
+  $ssl_certificate = undef,
+  $ssl_private_key = undef,
+  $ensure = 'present',
+  $deploy_user = 'web-deploy',
+  $deploy_user_authorized_keys = [],
+  $hooks = {},
+) {
+
+  include adblockplus::web
+  include nginx
+  include ssh
+
+  File {
+    mode => '0755',
+    owner => $deploy_user,
+    group => $deploy_user,
+  }
+
+  ensure_resource('file', "/var/www/$domain", {
+    ensure => ensure_directory_state($ensure),
+    owner => 'www-data',
+    group => 'www-data',
+  })
+
+  ensure_resource('nginx::hostconfig', $title, {
+    content => template('adblockplus/web/static.conf.erb'),
+    certificate => $ssl_certificate,
+    domain => $domain,
+    is_default => true,
+    private_key => $ssl_private_key,
+    log => 'web.access.log',
+  })
+
+  $content = [
+    "Match User ${deploy_user}",
+    'AllowTcpForwarding no',
+    'X11Forwarding no',
+    'AllowAgentForwarding no',
+    'GatewayPorts no',
+    'ForceCommand /usr/local/bin/hooks_wrapper $SSH_ORIGINAL_COMMAND',
+  ]
+
+  ensure_resource('concat::fragment', 'helpcenter', {
+    content => join($content, "\n\t"),
+    ensure => 'present',
+    target => 'sshd_config',
+    order => '20',
+  })
+
+  ensure_resource('adblockplus::user', $deploy_user, {
+    authorized_keys => $deploy_user_authorized_keys,
+    ensure => $ensure,
+    shell => '/bin/bash',
+    groups => ['www-data'],
+  })
+
+  $wrapper_path = "/home/${deploy_user}/bin"
+  ensure_resource('file', 'commands_dir', {
+    path => $wrapper_path,
+    ensure => ensure_directory_state($ensure),
+  })
+
+  ensure_resource('file', '/usr/local/bin/hooks_wrapper', {
+    ensure => ensure_file_state($ensure),
+    content => template('adblockplus/web/hooks_wrapper.sh.erb'),
+  })
+
+  # https://docs.puppet.com/puppet/latest/function.html#createresources
+  create_resources('adblockplus::web::static::hook', $hooks)
+}
+

modules/adblockplus/manifests/web/static/hook.pp
@@ -1,0 +1,47 @@
+# == Type: adblockplus::web::static::hook
+#
+# Manage custom hooks to be triggered via ssh commands
+#
+# === Parameters:
+#
+# [*file*]
+#   Overwrite group and the source of the content of the file.
+#
+# === Examples:
+#
+#  adblockplus::web::static::hook {'deploy':
+#    file => {
+#      source => 'puppet:///modules/adblockplus/web/deploy.py',
+#      path => '/usr/local/bin/deploy.py',
+#    },
+#   }
+#
+#  adblockplus::web::static::hook {'uname':
+#    file => {
+#      content => 'uname -a',
+#    },
+#  }
+#
+#  adblockplus::web::static::hook {'uptime':
+#    file => {
+#      target => '/usr/bin/uptime',
+#      ensure => 'link',
+#    },
+#  }
+#
+define adblockplus::web::static::hook (
+  $file,
+) {
+
+  $hook_path = "/home/${adblockplus::web::static::deploy_user}/bin/${name}"
+
+  ensure_resource('file', "web-deploy-hook#${title}", merge({
+    group => $adblockplus::web::static::deploy_user,
+    ensure => ensure_file_state($adblockplus::web::static::ensure),
+  }, $file, {
+    mode => '0755',
+    owner => $adblockplus::web::static::deploy_user,
+    path => $hook_path,
+  }))
+}
+

modules/adblockplus/templates/web/hooks_wrapper.sh.erb
@@ -1,0 +1,31 @@
+#!/bin/sh
+# Puppet: <%= @title %>
+# This script is a wrapper of accesible hooks, defined via hiera and accessed
+# via ssh with the ForceCommand option.
+
+usage()
+{
+  echo "Avaiable commands are:"
+  echo "help"
+<% @hooks.keys.each do |key| -%>
+  echo "<%= key %>"
+<% end -%>
+}
+
+command="$1"; shift
+case "$command" in
+  "help")
+    usage
+    ;;
+<% @hooks.each do |command, values| -%>
+"<%= command %>")
+    "<%= File.join(@wrapper_path, command) %>" "$@"
+    ;;
+<% end -%>
+  *)
+    echo "Unrecognized command"
+    usage
+    exit 1
+    ;;
+esac
+

modules/adblockplus/templates/web/static.conf.erb
@@ -1,0 +1,80 @@
+# Puppet: <%= @title %>
+
+root /var/www/<%= @domain %>;
+index index;
+default_type text/html;
+charset utf-8;
+
+set $index_page "index";
+
+<% if @custom_config %>
+<%=   @custom_config %>
+<% end %>
+
+location /
+{
+  expires 1d;
+
+  # Redirect server/language root
+
+  rewrite ^/$ /$index_page last;
+  rewrite ^/([a-z][a-z](_[A-Z][A-Z])?)$ /$1/ permanent;
+  rewrite ^/([a-z][a-z](_[A-Z][A-Z])?)/$ /$1/$index_page last;
+
+  # Match Accept-Language header against available languages
+
+  set $language en;
+  set $region "";
+
+  if ($http_accept_language ~ ^([a-z][a-z])\b)
+  {
+    set $language $1;
+  }
+  if ($http_accept_language ~ ^([a-z][a-z])-([A-Z][A-Z])\b)
+  {
+    set $region $1;
+  }
+
+  if ($arg_fb_locale ~ ^([a-z][a-z])_([A-Z][A-Z])$)
+  {
+    set $language $1;
+    set $region $2;
+  }
+
+  # Redirect canonical URLs to language-specific versions
+
+  if (-e "$document_root/${language}_$region$uri")
+  {
+    rewrite ^(.*) /${language}_$region last;
+  }
+  if (-e "$document_root/$language$uri")
+  {
+    rewrite ^(.*) /$language$1 last;
+  }
+  if (-e "$document_root/en$uri")
+  {
+    rewrite ^(.*) /en$1 last;
+  }
+}
+
+# Redirect language URIs if no translations are found for the requested page
+
+location ~ ^/([a-z][a-z]\_[A-Z][A-Z])(/.+)
+{
+  if (!-e "$document_root$uri")
+  {
+    # if there is no language+region translation, try parent language URI
+    # example /es_MX/page -> /es/page
+    rewrite ^/([a-z][a-z])\_([A-Z][A-Z])(/.+) /$1$3 redirect;
+  }
+}
+
+location ~ ^/([a-z][a-z])(/.+)
+{
+  if (!-e "$document_root$uri")
+  {
+    # if there is no language translation, try canonical page for default language
+    # example /es/page -> /page
+    rewrite ^/([a-z][a-z])(/.+) $2 redirect;
+  }
+}

commit 6a3d08d42f9419cd85ec80cf165b8b53959e198b

Remove global declaration of owner, group and mode for files on static websites
This bug was introduced in 0639b91b983e8c5c64555a03db367e316421cfd6

The File declaration (note the capital F) tells puppet to use those values for
all the file resources within scope, breaking the logrotation due wrong mode,
owner and group, correct values are set on the logrotate module.

Please refer to: https://jira.eyeo.com/browse/HD-654 for more information.

modules/adblockplus/manifests/web/static.pp
@@ -62,12 +62,6 @@ class adblockplus::web::static (
   include nginx
   include ssh
 
-  File {
-    mode => '0755',
-    owner => $deploy_user,
-    group => $deploy_user,
-  }
-
   ensure_resource('file', "/var/www/$domain", {
     ensure => ensure_directory_state($ensure),
     owner => 'www-data',
@@ -110,11 +104,17 @@ class adblockplus::web::static (
   ensure_resource('file', 'commands_dir', {
     path => $wrapper_path,
     ensure => ensure_directory_state($ensure),
+    mode => '0755',
+    owner => $deploy_user,
+    group => $deploy_user,
   })
 
   ensure_resource('file', '/usr/local/bin/hooks_wrapper', {
     ensure => ensure_file_state($ensure),
     content => template('adblockplus/web/hooks_wrapper.sh.erb'),
+    mode => '0755',
+    owner => $deploy_user,
+    group => $deploy_user,
   })
 
   # https://docs.puppet.com/puppet/latest/function.html#createresources